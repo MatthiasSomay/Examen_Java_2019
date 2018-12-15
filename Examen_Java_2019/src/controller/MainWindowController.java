65e87d1cfb52b6a52c98313a0cefeac073d52f47
matthiassomay <matthiassomay@gmail.com>
2018-12-15 12:57:48 +0100

    Headers in commentaar

--- Examen_Java_2019/src/controller/MainWindowController.java
+++ Examen_Java_2019/src/controller/MainWindowController.java
@@ -1,3 +1,10 @@
+/**
+ * @Autor: Matthias Somay & Kenneth Van De Borne
+ * @Date: 15/12/2018
+ * @Project: Examen_Java_2019
+ * @Purpose: Controller klasse JavaFX
+ */
+
 package controller;
 
 public class MainWindowController {
--- Examen_Java_2019/src/model/Vervoermiddel.java
+++ Examen_Java_2019/src/model/Vervoermiddel.java
@@ -1,3 +1,12 @@
+/**
+ * @Autor: Matthias Somay & Kenneth Van De Borne
+ * @Date: 15/12/2018
+ * @Project: Examen_Java_2019
+ * @Purpose: Vervoermiddel klasse
+ */
+
+
+
 package model;
 
 public class Vervoermiddel {
