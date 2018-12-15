75cd7e3c7fba9bd8b505e1d0a827c40bff2b8680
matthiassomay <matthiassomay@gmail.com>
2018-12-15 12:50:06 +0100

    Fix

--- Examen_Java_2019/src/view/MainWindow.java
+++ Examen_Java_2019/src/view/MainWindow.java
@@ -6,7 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class MainWIndow extends Application {
+public class MainWindow extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
