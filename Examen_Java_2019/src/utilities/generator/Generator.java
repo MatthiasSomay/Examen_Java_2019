417c8192414b7a2774da210993013db6f39dccac
kennethvdborne <44980914+kennethvdborne@users.noreply.github.com>
2018-12-22 20:22:22 +0100

    Generator update

--- Examen_Java_2019/src/utilities/generator/Generator.java
+++ Examen_Java_2019/src/utilities/generator/Generator.java
@@ -5,6 +5,8 @@ import utilities.demodata.HulpdienstTypeLijst;
 import utilities.demodata.SchipTypeLijst;
 import utilities.demodata.VerkeerstorenTypeLijst;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.Random;
 
 public class Generator {
@@ -29,27 +31,73 @@ public class Generator {
         return new Coördinaten(((1+ (Math.random()*10))), ((1+ (Math.random()*10))));
     }
 
-    public double generateSnelheid(){
-        return (1+ (Math.random()*10));
+    public double generateSnelheid(String Type){
+        switch (Type) {
+            case "Tanker": return (13+ (Math.random()*4));
+            case "Cruiseschip": return (18+ (Math.random()*4));
+            case "ContainerSchip": return (25+ (Math.random()*8));
+            case "Zeilboot": return (6+ (Math.random()*4));
+            case "Speedboot": return (30+ (Math.random()*20));
+            case "Vissersboot": return (13+ (Math.random()*4));
+            case "Seaking": return (140+ (Math.random()*40));
+            case "Zeepolitie": return (85+ (Math.random()*20));
+            case "Marine": return (40+ (Math.random()*20));
+            case "Zeebrandweer": return (65+ (Math.random()*20));
+            default: return 0;
+        }
     }
 
-    public double generateGrootte(){
-        return (50+ (Math.random()*200));
+    public double generateGrootte(String Type){
+        switch (Type) {
+            case "Tanker": return (25000+ (Math.random()*10000));
+            case "Cruiseschip": return (10000+ (Math.random()*5000));
+            case "ContainerSchip": return (15000+ (Math.random()*10000));
+            case "Zeilboot": return (30+ (Math.random()*15));
+            case "Speedboot": return (20+ (Math.random()*60));
+            case "Vissersboot": return (150+ (Math.random()*100));
+            case "Seaking": return (8+ (Math.random()*3));
+            case "Zeepolitie": return (60+ (Math.random()*20));
+            case "Marine": return (2000+ (Math.random()*10000));
+            case "Zeebrandweer": return (60+ (Math.random()*20));
+            default: return 0;
+        }
     }
 
-    public double generateWendbaarheid(){
-        return (50+ (Math.random()*200));
+    public double generateWendbaarheid(String Type){
+        switch (Type) {
+            case "Tanker": return (7+ (Math.random()*4));
+            case "Cruiseschip": return (5+ (Math.random()*3));
+            case "ContainerSchip": return (6+ (Math.random()*4));
+            case "Zeilboot": return (0.6+ (Math.random()*0.4));
+            case "Speedboot": return (0.09+ (Math.random()*0.05));
+            case "Vissersboot": return (3+ (Math.random()*5));
+            case "Seaking": return (0.09+ (Math.random()*0.05));
+            case "Zeepolitie": return (0.25+ (Math.random()*0.1));
+            case "Marine": return (5+ (Math.random()*3));
+            case "Zeebrandweer": return (0.35+ (Math.random()*0.15));
+            default: return 0;
+        }
     }
 
-    public int generateCapaciteit(){
-        return (50+ (int)(Math.random()*200));
-    }
-
-    public int generatePersonenAanBoord(){
-        return (50+ (int)(Math.random()*200));
+    public int generatePersonenAanBoord(String Type){
+        DecimalFormat df = new DecimalFormat("#");
+        df.setRoundingMode(RoundingMode.CEILING);
+        switch (Type) {
+            case "Tanker": return Integer.parseInt(df.format(20+ (Math.random()*10)));
+            case "Cruiseschip": return Integer.parseInt(df.format(500+ (Math.random()*6000)));
+            case "ContainerSchip": return Integer.parseInt(df.format(30+ (Math.random()*15)));
+            case "Zeilboot": return Integer.parseInt(df.format(2+ (Math.random()*8)));
+            case "Speedboot": return Integer.parseInt(df.format(2+ (Math.random()*8)));
+            case "Vissersboot": return Integer.parseInt(df.format(10+ (Math.random()*5)));
+            case "Seaking": return Integer.parseInt(df.format(3+ (Math.random()*2)));
+            case "Zeepolitie": return Integer.parseInt(df.format(7+ (Math.random()*3)));
+            case "Marine": return Integer.parseInt(df.format(1500+ (Math.random()*500)));
+            case "Zeebrandweer": return Integer.parseInt(df.format(7+ (Math.random()*10)));
+            default: return 0;
+        }
     }
 
     public double generateKoers(){
-        return (50+ (Math.random()*200));
+        return ((Math.random()*360));
     }
 }
