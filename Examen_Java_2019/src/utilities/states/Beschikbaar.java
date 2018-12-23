3ca1558b9c5b410dd85add33ba3d462c80f72b46
matthiassomay <matthiassomay@gmail.com>
2018-12-23 11:23:25 +0100

    State pattern

--- Examen_Java_2019/src/utilities/states/Beschikbaar.java
+++ Examen_Java_2019/src/utilities/states/Beschikbaar.java
@@ -1,3 +1,10 @@
+/**
+ * @Autor: Matthias Somay & Kenneth Van De Borne
+ * @Date: 23/12/2018
+ * @Project: Examen_Java_2019
+ * @Purpose: State Pattern - Beschikbaar state klasse
+ */
+
 package utilities.states;
 
 import utilities.interfaces.IStatus;
--- Examen_Java_2019/src/utilities/states/NietBeschikbaar.java
+++ Examen_Java_2019/src/utilities/states/NietBeschikbaar.java
@@ -1,3 +1,11 @@
+/**
+ * @Autor: Matthias Somay & Kenneth Van De Borne
+ * @Date: 23/12/2018
+ * @Project: Examen_Java_2019
+ * @Purpose: State Pattern - Niet Beschikbaar state klasse
+ */
+
+
 package utilities.states;
 
 import utilities.interfaces.IStatus;
