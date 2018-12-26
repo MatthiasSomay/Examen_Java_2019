d7e94e46543a1024c270f58d1d6794622006b32a
matthiassomay <matthiassomay@gmail.com>
2018-12-26 19:37:00 +0100

    junit - aanmaak VerkeerstorenTest

---
+++ Examen_Java_2019/src/test/VerkeerstorenTest.java
@@ -0,0 +1,51 @@
+/**
+ * @Autor: Matthias Somay & Kenneth Van De Borne
+ * @Date: 26/12/2018
+ * @Project: Examen_Java_2019
+ * @Purpose: Verkeerstoren testklasse
+ */
+
+package test;
+
+import model.Coördinaten;
+import model.Verkeerstoren;
+import org.junit.Test;
+
+public class VerkeerstorenTest {
+
+    private Coördinaten coordinaat = new Coördinaten(10,20);
+    private Verkeerstoren verkeerstoren = new Verkeerstoren();
+
+    @Test
+    public void addSchipObserver() {
+    }
+
+    @Test
+    public void removeSchipObserver() {
+    }
+
+    @Test
+    public void noodsituatieBroadcastBericht() {
+    }
+
+    @Test
+    public void berekenAfstand() {
+    }
+
+    @Test
+    public void verleenHulp() {
+    }
+
+    @Test
+    public void setHulpdiensten() {
+    }
+
+    @Test
+    public void setSchepen() {
+    }
+
+    @Test
+    public void test_setType_Geldige_Waarde_Wordt_Aanvaard() {
+
+    }
+}
