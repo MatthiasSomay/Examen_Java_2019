935981f80b16e995b918a152a92eee5e6ff1133a
matthiassomay <matthiassomay@gmail.com>
2019-01-01 22:25:40 +0100

    Update

--- Examen_Java_2019/src/db/DatabaseQueries.java
+++ Examen_Java_2019/src/db/DatabaseQueries.java
@@ -7,10 +7,11 @@
 
 package db;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import model.Schip;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DatabaseQueries {
     private static final String URL = "jdbc:mysql://localhost:3306/Examen_Java";
@@ -19,7 +20,10 @@ public class DatabaseQueries {
 
     private Connection connection;
     private PreparedStatement selectAllShips;
+    private PreparedStatement insertNewVervoermiddel;
+    private PreparedStatement insertNewActor;
     private PreparedStatement insertNewShip;
+    private PreparedStatement insertNewLocation;
 
     // TODO: 2018-12-21  
     public DatabaseQueries() {
@@ -29,13 +33,30 @@ public class DatabaseQueries {
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
 
             selectAllShips = connection.prepareStatement(
-                    "SELECT * FROM Schip s " +
+                    "SELECT * FROM Vervoermiddel v, Schip s " +
+                            "WHERE s.vervoermiddelID = v.vervoermiddelID" +
                             "ORDER BY s.schipID "
             );
 
+            insertNewLocation = connection.prepareStatement(
+                    "INSERT INTO Locatie" + "(lengte, breedte)" + "VALUES (?, ?)"
+            );
+
+            insertNewActor = connection.prepareStatement(
+                    "INSERT INTO Actor" +
+                            "(locatieID)" + "VALUES (?)"
+            );
+
+            insertNewVervoermiddel = connection.prepareStatement(
+                    "INSERT INTO Vervoermiddel " +
+                            "(actorID, snelheid, grootte, wendbaarheid, capaciteit, personenAanBoord, koers) " +
+                            "VALUES (?, ?, ?, ?, ?, ?, ?) "
+
+            );
+
             insertNewShip = connection.prepareStatement(
-                    "INSERT INTO Schip " +
-                            ""
+                    "INSERT INTO Schip" +
+                            "(vervoermiddelID, status) " + "VALUES (?,?)"
             );
 
         }
@@ -43,6 +64,27 @@ public class DatabaseQueries {
         catch (SQLException e) {
             e.printStackTrace();
 
+        }
+
+        public List<Schip> getAllShips() {
+            try (ResultSet resultSet = selectAllShips.executeQuery()) {
+                List<Schip> schips = new ArrayList<Schip>();
+
+                while(resultSet.next()) {
+                    schips.add(new Schip(
+                            resultSet.getInt("locatie"),
+                            resultSet.getInt("schipID"),
+                            resultSet.getInt("vervoermiddelID"),
+                            resultSet.getString("status")
+                    ));
+                }
+
+            }
+
+            catch (SQLException e){
+                e.printStackTrace();
+
+            }
         }
 
 
