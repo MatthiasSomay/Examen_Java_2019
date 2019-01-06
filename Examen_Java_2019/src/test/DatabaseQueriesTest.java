15953608fa723326bf85bbb533e4268d127a705f
matthiassomay <matthiassomay@gmail.com>
2019-01-06 14:39:28 +0100

    DBTEST

---
+++ Examen_Java_2019/src/test/DatabaseQueriesTest.java
@@ -0,0 +1,42 @@
+package test;
+
+import org.junit.Test;
+
+public class DatabaseQueriesTest {
+
+    @Test
+    public void calculateState() {
+    }
+
+    @Test
+    public void getAllSchip() {
+    }
+
+    @Test
+    public void getAllHulpdienst() {
+    }
+
+    @Test
+    public void getAllVerkeerstoren() {
+    }
+
+    @Test
+    public void addVervoermiddel() {
+    }
+
+    @Test
+    public void addVerkeerstoren() {
+    }
+
+    @Test
+    public void addVerkeerstoren1() {
+    }
+
+    @Test
+    public void deleteVervoermiddel() {
+    }
+
+    @Test
+    public void deleteVerkeerstoren() {
+    }
+}
